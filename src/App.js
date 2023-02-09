611db14f9df20d214bc64c3e4275b69edefcd522
Bravetomato <princeps2nd@gmail.com>
2023-02-09 20:53:03 +0900

    지마켓 산스 폰트, 폰트어썸, 테일윈드, MUI, 리액트 라우터, 리코일, classnames, immer설치

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { AppBar, Toolbar } from "@mui/material";
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Sub1Page from "./pages/Sub1Page";
+
 function App() {
   return (
     <>
-    <div>안녕</div>
+      <AppBar position="static">
+        <Toolbar className="justify-center">
+          <div className="flex-1"></div>
+          <span className="font-bold">Todo List</span>
+          <div className="flex-1"></div>
+        </Toolbar>
+      </AppBar>
+      <Routes>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/sub1" element={<Sub1Page />} />
+        <Route path="*" element={<Navigate to="/main"/>} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Sub1Page.js
@@ -0,0 +1,9 @@
+function Sub1Page() {
+    return (
+      <>
+        <h1>서브 페이지</h1>
+      </>
+    );
+  }
+
+  export default Sub1Page;
